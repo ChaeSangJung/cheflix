439a1879719f531e8e60f99a624991c80d712c3d
CheSangJung <shtingstar@hanmail.net>
2021-07-08 10:04:32 +0900

    Updates

---
+++ precache-manifest.120b172d95718c85c0f596e4a0ebf20c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5ea953e3de948969123",
+    "url": "/cheflix/static/js/main.c5ea953e.chunk.js"
+  },
+  {
+    "revision": "9421c7cb3ea4280b2b34",
+    "url": "/cheflix/static/css/1.46c77477.chunk.css"
+  },
+  {
+    "revision": "9421c7cb3ea4280b2b34",
+    "url": "/cheflix/static/js/1.9421c7cb.chunk.js"
+  },
+  {
+    "revision": "7c71474322b2f25805b8",
+    "url": "/cheflix/static/js/runtime~main.7c714743.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/cheflix/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "52e4201e15b601039a0306a40b5f49c8",
+    "url": "/cheflix/static/media/user.52e4201e.png"
+  },
+  {
+    "revision": "3779860b0aa48578448e9f52e247b7b1",
+    "url": "/cheflix/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a4b6712bc6860691807ac790e696209.js"
+  "/cheflix/precache-manifest.120b172d95718c85c0f596e4a0ebf20c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/cheflix/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
