4e11b1a2ef307531510bf9e048b5515b337f16d6
CheSangJung <shtingstar@hanmail.net>
2021-07-08 10:02:35 +0900

    Updates

---
+++ precache-manifest.2a4b6712bc6860691807ac790e696209.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5ea953e3de948969123",
+    "url": "/static/js/main.c5ea953e.chunk.js"
+  },
+  {
+    "revision": "07a3bf198d1e65d46082",
+    "url": "/static/css/1.46c77477.chunk.css"
+  },
+  {
+    "revision": "07a3bf198d1e65d46082",
+    "url": "/static/js/1.07a3bf19.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "52e4201e15b601039a0306a40b5f49c8",
+    "url": "/static/media/user.52e4201e.png"
+  },
+  {
+    "revision": "880d94d07af9917a60651b98cdcbc847",
+    "url": "/index.html"
+  }
+];
